458afa54522882ce557cb29924117a12994d1357
felixtanhm <felixtanhm@gmail.com>
2024-02-06 16:37:42 +0800

    Main page title

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <PlayerProvider>
       <div className="min-h-full flex flex-col items-center justify-between">
+        <div className="flex flex-col text-color items-center p-6">
+          <span className="font-bold text-2xl">🕹️🎲🎮</span>
+          <p className="font-bold text-xl sm:text-2xl">Game Arcade</p>
+        </div>
         <Nav route="/" />
         <Footer />
       </div>
