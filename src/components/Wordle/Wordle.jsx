d29d32fdc750ba9bd0d31a41d8f093cc76f6ac76
felixtanhm <felixtanhm@gmail.com>
2024-02-08 22:17:51 +0800

    Wordle Endgame - Implemented dialog to show win / lose - Implemented reset game function

--- src/components/Wordle/Wordle.jsx
+++ src/components/Wordle/Wordle.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GuessDisplay from "./GuessDisplay";
 import GuessForm from "./GuessForm";
+import DialogTW from "../DialogTW";
 import { WORDS } from "../../utils/constants";
 import { randomInt } from "../../utils/helperFunctions";
 
@@ -9,7 +10,10 @@ function Wordle() {
     WORDS[randomInt(WORDS.length, "floor")]
   );
   const [guessList, setGuessList] = React.useState([]);
+  const [gameStatus, setGameStatus] = React.useState("pending");
   console.log(answer);
+
+  function validateGuess(guess) {}
 
   function updateGuessList(guess) {
     const nextGuessList = [...guessList];
@@ -17,10 +21,29 @@ function Wordle() {
     setGuessList(nextGuessList);
   }
 
+  function resetGame() {
+    setAnswer(WORDS[randomInt(WORDS.length, "floor")]);
+    setGuessList([]);
+    setGameStatus("pending");
+  }
+
   return (
     <div>
       <GuessDisplay data={guessList} />
       <GuessForm updateGuessList={updateGuessList} />
+      {gameStatus !== "pending" && (
+        <DialogTW
+          icon={gameStatus === "success" ? "🎉" : "👾"}
+          title={
+            gameStatus === "success"
+              ? "You guessed the word!"
+              : "You're out of guesses..."
+          }
+          description={"Would you like to play again?"}
+          buttonCTA={"Play Again"}
+          handleClick={resetGame}
+        />
+      )}
     </div>
   );
 }
