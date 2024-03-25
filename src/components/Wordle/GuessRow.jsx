569c4e5a62144da85eea7eeb7e1766c96bff3676
felixtanhm <felixtanhm@gmail.com>
2024-03-26 05:53:57 +0800

    update seo desc

--- src/components/Wordle/GuessRow.jsx
+++ src/components/Wordle/GuessRow.jsx
@@ -3,15 +3,16 @@ import { WORD_LENGTH } from "../../utils/constants";
 
 function GuessRow({ guess }) {
   const letters = range(WORD_LENGTH);
+  const statusColor = {
+    correct: "bg-emerald-400 dark:bg-emerald-600/50",
+    incorrect: "bg-slate-300 dark:bg-white/20",
+    misplaced: "bg-amber-400 dark:bg-amber-400/50",
+  };
+
   if (guess) {
     return (
       <div id="guessRow" className="grid grid-cols-5 gap-1">
         {letters.map((char) => {
-          const statusColor = {
-            correct: "bg-emerald-400 dark:bg-emerald-600/50",
-            incorrect: "bg-slate-300 dark:bg-white/20",
-            misplaced: "bg-amber-400 dark:bg-amber-400/50",
-          };
           return (
             <span
               key={char}
